42e1d180088f4f754413d7d6f6cc6157d4c98c9f
ghaydarafa <ghayda.rafa@gmail.com>
2025-02-10 20:18:29 +0700

    data dummy seed

---
+++ ecommerce_seed.sql
@@ -0,0 +1,213 @@
+--
+-- Data for Name: addresses; Type: TABLE DATA; 
+--
+
+INSERT INTO addresses (address_id, building_name, city, country, pincode, state, street) VALUES (8, 'Apartemen Harmoni', 'Jakarta', 'Indonesia', '101112', 'DKI Jakarta', 'Jl. Sudirman No. 123');
+INSERT INTO addresses (address_id, building_name, city, country, pincode, state, street) VALUES (9, 'Grand Residence', 'Bandung', 'Indonesia', '402112', 'Jawa Barat', 'Jl. Thamrin No. 45');
+INSERT INTO addresses (address_id, building_name, city, country, pincode, state, street) VALUES (10, 'Merdeka Tower', 'Surabaya', 'Indonesia', '601223', 'Jawa Timur', 'Jl. Merdeka No. 12');
+INSERT INTO addresses (address_id, building_name, city, country, pincode, state, street) VALUES (13, 'Kampus Tower', 'Malang', 'Indonesia', '601223', 'Jawa Timur', 'Jl. Kampus No. 10');
+INSERT INTO addresses (address_id, building_name, city, country, pincode, state, street) VALUES (14, 'Komplek TNI', 'Magelang', 'Indonesia', '901923', 'Jawa Tengah', 'Jl. Panjang No. 2');
+INSERT INTO addresses (address_id, building_name, city, country, pincode, state, street) VALUES (15, 'Komplek TNI', 'Magelang', 'Indonesia', '901923', 'Jawa Tengah', 'Jl. Angklung No. 1');
+INSERT INTO addresses (address_id, building_name, city, country, pincode, state, street) VALUES (16, 'Komplek Cascade', 'Depok', 'Indonesia', '163444', 'Jawa Barat', 'Jl. New Jersey No. 14');
+INSERT INTO addresses (address_id, building_name, city, country, pincode, state, street) VALUES (17, 'Komplek Elite', 'Bogor', 'Indonesia', '301923', 'Jawa Barat', 'Jl. Piano No. 1');
+INSERT INTO addresses (address_id, building_name, city, country, pincode, state, street) VALUES (18, 'Komplek Elite', 'Bogor', 'Indonesia', '301923', 'Jawa Barat', 'Jl. Baru No. 7');
+INSERT INTO addresses (address_id, building_name, city, country, pincode, state, street) VALUES (19, 'Komplek Elite', 'Bogor', 'Indonesia', '301923', 'Jawa Tengah', 'Jl. Rusuh No. 2');
+INSERT INTO addresses (address_id, building_name, city, country, pincode, state, street) VALUES (21, 'Komplek Elite', 'Bogor', 'Indonesia', '301923', 'Jawa Barat', 'Jl. Rusuh No. 2');
+INSERT INTO addresses (address_id, building_name, city, country, pincode, state, street) VALUES (22, 'Komplek Legend', 'Bogor', 'Indonesia', '301923', 'Jawa Barat', 'Jl. moba No. 2');
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; 
+--
+
+INSERT INTO users (user_id, email, first_name, last_name, mobile_number, password) VALUES (1, 'jac@gmail.com', 'Jacqueline', 'Hathaway', '0812345678', '$2a$10$imBlylz9FvofQHAHaih3qOw/zRNAu6WmZU.7z/rwnAhlHaywSoZ9y');
+INSERT INTO users (user_id, email, first_name, last_name, mobile_number, password) VALUES (2, 'michael@gmail.com', 'Michael', 'Anderson', '0812345679', '$2a$10$FW/dh7FqbJQYN.Lc0XtVBOoy8r7wVEGwY.JWzZZzI2aEl4Yk0mA7i');
+INSERT INTO users (user_id, email, first_name, last_name, mobile_number, password) VALUES (3, 'admin2@gmail.com', 'Admin', 'Kedua', '0823345680', '$2a$10$wInFGAcbRgbbD.EIvbZU8egnPqpbGrie/dFnlOVAn6tl0s0tpZHsu');
+INSERT INTO users (user_id, email, first_name, last_name, mobile_number, password) VALUES (4, 'admin@gmail.com', 'Admin', 'Admin', '0822345680', '$2a$10$qebot2hU80hl/V2eJawVZ.k7Ay82H2a/xQ6EOVAojF0ybTTUOODNm');
+INSERT INTO users (user_id, email, first_name, last_name, mobile_number, password) VALUES (5, 'emily@gmail.com', 'Emily', 'Clark', '0812345680', '$2a$10$VSeR6caBozmwIfcvN0SEBODwRtXoct7OI4F7x5pCMaYjqtg2BOXcu');
+INSERT INTO users (user_id, email, first_name, last_name, mobile_number, password) VALUES (6, 'rosie@gmail.com', 'Rosie', 'Tjahya', '0811340680', '$2a$10$9ejVCidnlVxNpSffH4jBau5ZmM6b0UaUf7JxQFtSqNX051IaMW/Sm');
+INSERT INTO users (user_id, email, first_name, last_name, mobile_number, password) VALUES (7, 'nat@gmail.com', 'Natalie', 'Swaney', '0815340680', '$2a$10$CqmVnlx858mtzHFT47GuouihgoMK04e3O41E5GWIWgKSKD6xQGOOy');
+INSERT INTO users (user_id, email, first_name, last_name, mobile_number, password) VALUES (8, 'hanzo@gmail.com', 'Hanzo', 'Belerick', '0815340980', '$2a$10$0NPHA5xF0CNdx67nifFJMeDzWkTTmUJvb8wlMMWsMzPnXMG7Ke1wi');
+INSERT INTO users (user_id, email, first_name, last_name, mobile_number, password) VALUES (9, 'valen@gmail.com', 'Valen', 'Edgar', '0815311980', '$2a$10$7NDIiZgIfh.vsc56YHf5Xe4gAGfDGsQ05fO1Q1gkcKc5PnR7YuruW');
+INSERT INTO users (user_id, email, first_name, last_name, mobile_number, password) VALUES (10, 'odette@gmail.com', 'Odette', 'Kazuha', '0815301980', '$2a$10$tTKBaAob1S8EQhHwgtcxouuJQY.kLGx56BhBwpEQoZgJ31lDWdFA.');
+INSERT INTO users (user_id, email, first_name, last_name, mobile_number, password) VALUES (12, 'kalei@gmail.com', 'Kalei', 'Darmawan', '0815391980', '$2a$10$ptacf6EyWYOEq13u83XHzu9Klf9akb9fMLdc81RioBrv2ZCbDARuS');
+
+
+--
+-- Data for Name: carts; Type: TABLE DATA; 
+--
+
+INSERT INTO carts (cart_id, total_price, user_id) VALUES (15, 0, 7);
+INSERT INTO carts (cart_id, total_price, user_id) VALUES (7, 0, 2);
+INSERT INTO carts (cart_id, total_price, user_id) VALUES (14, 0, 6);
+INSERT INTO carts (cart_id, total_price, user_id) VALUES (6, 0, 1);
+INSERT INTO carts (cart_id, total_price, user_id) VALUES (20, -999975000000, 10);
+INSERT INTO carts (cart_id, total_price, user_id) VALUES (21, 1000000, 12);
+INSERT INTO carts (cart_id, total_price, user_id) VALUES (17, 1600000, 9);
+INSERT INTO carts (cart_id, total_price, user_id) VALUES (8, 4000000, 3);
+INSERT INTO carts (cart_id, total_price, user_id) VALUES (9, 4600000, 4);
+INSERT INTO carts (cart_id, total_price, user_id) VALUES (10, 1500000, 5);
+INSERT INTO carts (cart_id, total_price, user_id) VALUES (16, 6500000, 8);
+
+
+--
+-- Data for Name: categories; Type: TABLE DATA; 
+--
+
+INSERT INTO categories (category_id, category_name) VALUES (1, 'Electronics');
+INSERT INTO categories (category_id, category_name) VALUES (2, 'Furniture');
+INSERT INTO categories (category_id, category_name) VALUES (3, 'Books');
+INSERT INTO categories (category_id, category_name) VALUES (4, 'Clothing');
+INSERT INTO categories (category_id, category_name) VALUES (5, 'Footwear');
+INSERT INTO categories (category_id, category_name) VALUES (6, 'Accessories');
+INSERT INTO categories (category_id, category_name) VALUES (7, 'Beauty & Health');
+INSERT INTO categories (category_id, category_name) VALUES (8, 'Sports');
+INSERT INTO categories (category_id, category_name) VALUES (9, 'Toys');
+INSERT INTO categories (category_id, category_name) VALUES (10, 'Groceries');
+INSERT INTO categories (category_id, category_name) VALUES (12, 'New Category');
+INSERT INTO categories (category_id, category_name) VALUES (13, 'Foods');
+
+
+--
+-- Data for Name: products; Type: TABLE DATA; 
+--
+
+INSERT INTO products (product_id, description, discount, image, price, product_name, quantity, special_price, category_id) VALUES (6, 'SSD NVMe kecepatan tinggi untuk gaming dan editing', 500000, 'https://example.com/ssd.jpg', 3000000, 'SSD Samsung 1TB NVMe', 25, 2500000, 5);
+INSERT INTO products (product_id, description, discount, image, price, product_name, quantity, special_price, category_id) VALUES (12, 'Mouse working wireless dengan sensor HERO 25K', 500000, '491e4a89-1c28-45d7-b7de-6b205a9c122e.png', 1500000, 'Mouse Apple', 15, 1000000, 2);
+INSERT INTO products (product_id, description, discount, image, price, product_name, quantity, special_price, category_id) VALUES (9, 'Kursi gaming ergonomis dengan bahan premium', 1000000, 'https://example.com/chair.jpg', 6000000, 'Gaming Chair Secretlab Titan', 4, 5000000, 8);
+INSERT INTO products (product_id, description, discount, image, price, product_name, quantity, special_price, category_id) VALUES (4, 'Monitor gaming 144Hz IPS 27 inch', 1000000, 'https://example.com/monitor.jpg', 5000000, 'Monitor LG UltraGear', 1, 4000000, 3);
+INSERT INTO products (product_id, description, discount, image, price, product_name, quantity, special_price, category_id) VALUES (13, 'laptop baru', 200000, 'default.png', 10500000, 'New laptop lagi', 19, -20989500000, 12);
+INSERT INTO products (product_id, description, discount, image, price, product_name, quantity, special_price, category_id) VALUES (1, 'Gaming laptop high performance', 5000000, '74fd3347-dd7c-414d-a7b3-89604147a98e.png', 20000000, 'Laptop Asus ROG update', 9, -999980000000, 1);
+INSERT INTO products (product_id, description, discount, image, price, product_name, quantity, special_price, category_id) VALUES (2, 'Mouse gaming wireless dengan sensor HERO 25K', 500000, 'https://example.com/mouse.jpg', 1500000, 'Mouse Logitech G Pro', 13, 1000000, 2);
+INSERT INTO products (product_id, description, discount, image, price, product_name, quantity, special_price, category_id) VALUES (8, 'Casing PC minimalis dengan kaca tempered', 300000, 'https://example.com/case.jpg', 1500000, 'PC Case NZXT H510', 4, 1200000, 7);
+INSERT INTO products (product_id, description, discount, image, price, product_name, quantity, special_price, category_id) VALUES (7, 'Power supply modular dengan sertifikasi 80+ Gold', 400000, 'https://example.com/psu.jpg', 2000000, 'Power Supply Corsair 750W', 7, 1600000, 6);
+INSERT INTO products (product_id, description, discount, image, price, product_name, quantity, special_price, category_id) VALUES (3, 'Keyboard gaming opto-mechanical switch', 500000, 'https://example.com/keyboard.jpg', 2500000, 'Keyboard Razer Huntsman', 7, 2000000, 2);
+INSERT INTO products (product_id, description, discount, image, price, product_name, quantity, special_price, category_id) VALUES (10, 'Webcam Full HD 1080p untuk streaming dan meeting', 200000, 'https://example.com/webcam.jpg', 1200000, 'Webcam Logitech C920', 15, 1000000, 9);
+INSERT INTO products (product_id, description, discount, image, price, product_name, quantity, special_price, category_id) VALUES (5, 'Headset gaming dengan suara surround 7.1', 300000, 'https://example.com/headset.jpg', 1800000, 'Headset HyperX Cloud II', 14, 1500000, 4);
+INSERT INTO products (product_id, description, discount, image, price, product_name, quantity, special_price, category_id) VALUES (11, 'laptop baru', 20000, 'default.png', 120000, 'New laptop', 17, -23880000, 12);
+
+
+--
+-- Data for Name: cart_items; Type: TABLE DATA; 
+--
+
+INSERT INTO cart_items (cart_item_id, discount, product_price, quantity, cart_id, product_id) VALUES (18, 1000000, 5000000, 1, 20, 9);
+INSERT INTO cart_items (cart_item_id, discount, product_price, quantity, cart_id, product_id) VALUES (19, 1000000, 5000000, 1, 16, 9);
+INSERT INTO cart_items (cart_item_id, discount, product_price, quantity, cart_id, product_id) VALUES (22, 5000000, -999980000000, 1, 20, 1);
+INSERT INTO cart_items (cart_item_id, discount, product_price, quantity, cart_id, product_id) VALUES (23, 500000, 1000000, 1, 21, 2);
+INSERT INTO cart_items (cart_item_id, discount, product_price, quantity, cart_id, product_id) VALUES (24, 400000, 1600000, 1, 17, 7);
+INSERT INTO cart_items (cart_item_id, discount, product_price, quantity, cart_id, product_id) VALUES (25, 400000, 1600000, 1, 9, 7);
+INSERT INTO cart_items (cart_item_id, discount, product_price, quantity, cart_id, product_id) VALUES (26, 500000, 2000000, 2, 8, 3);
+INSERT INTO cart_items (cart_item_id, discount, product_price, quantity, cart_id, product_id) VALUES (27, 300000, 1500000, 2, 9, 5);
+INSERT INTO cart_items (cart_item_id, discount, product_price, quantity, cart_id, product_id) VALUES (28, 300000, 1500000, 1, 10, 5);
+INSERT INTO cart_items (cart_item_id, discount, product_price, quantity, cart_id, product_id) VALUES (29, 300000, 1500000, 1, 16, 5);
+
+
+--
+-- Data for Name: payments; Type: TABLE DATA; 
+--
+
+INSERT INTO payments (payment_id, payment_method) VALUES (1, 'qris');
+INSERT INTO payments (payment_id, payment_method) VALUES (3, 'ecommercepay');
+INSERT INTO payments (payment_id, payment_method) VALUES (5, 'transfer-mandiri');
+INSERT INTO payments (payment_id, payment_method) VALUES (2, 'transfer-bsi');
+INSERT INTO payments (payment_id, payment_method) VALUES (4, 'transfer-bca');
+INSERT INTO payments (payment_id, payment_method) VALUES (6, 'transfer-bri');
+INSERT INTO payments (payment_id, payment_method) VALUES (7, 'transfer-bni');
+INSERT INTO payments (payment_id, payment_method) VALUES (8, 'transfer-maybank');
+INSERT INTO payments (payment_id, payment_method) VALUES (9, 'transfer-bni');
+INSERT INTO payments (payment_id, payment_method) VALUES (10, 'transfer-bni');
+INSERT INTO payments (payment_id, payment_method) VALUES (11, 'qris');
+
+
+--
+-- Data for Name: orders; Type: TABLE DATA; 
+--
+
+INSERT INTO orders (order_id, email, order_date, order_status, total_amount, payment_id) VALUES (1, 'emily@gmail.com', '2025-02-10', 'shipped', 7000000, 1);
+INSERT INTO orders (order_id, email, order_date, order_status, total_amount, payment_id) VALUES (2, 'emily@gmail.com', '2025-02-10', 'Order Accepted !', -20989500000, 2);
+INSERT INTO orders (order_id, email, order_date, order_status, total_amount, payment_id) VALUES (3, 'nat@gmail.com', '2025-02-10', 'Order Accepted !', -22680000, 3);
+INSERT INTO orders (order_id, email, order_date, order_status, total_amount, payment_id) VALUES (4, 'michael@gmail.com', '2025-02-10', 'Order Accepted !', 2600000, 4);
+INSERT INTO orders (order_id, email, order_date, order_status, total_amount, payment_id) VALUES (5, 'hanzo@gmail.com', '2025-02-10', 'Order Accepted !', 3000000, 5);
+INSERT INTO orders (order_id, email, order_date, order_status, total_amount, payment_id) VALUES (6, 'admin2@gmail.com', '2025-02-10', 'Order Accepted !', 1500000, 6);
+INSERT INTO orders (order_id, email, order_date, order_status, total_amount, payment_id) VALUES (7, 'admin@gmail.com', '2025-02-10', 'Order Accepted !', 8000000, 7);
+INSERT INTO orders (order_id, email, order_date, order_status, total_amount, payment_id) VALUES (8, 'rosie@gmail.com', '2025-02-10', 'Order Accepted !', 12000000, 8);
+INSERT INTO orders (order_id, email, order_date, order_status, total_amount, payment_id) VALUES (9, 'valen@gmail.com', '2025-02-10', 'Order Accepted !', 4000000, 9);
+INSERT INTO orders (order_id, email, order_date, order_status, total_amount, payment_id) VALUES (10, 'emily@gmail.com', '2025-02-10', 'Order Accepted !', 4000000, 10);
+INSERT INTO orders (order_id, email, order_date, order_status, total_amount, payment_id) VALUES (11, 'jac@gmail.com', '2025-02-10', 'Order Accepted !', 5500000, 11);
+
+
+--
+-- Data for Name: order_items; Type: TABLE DATA; 
+--
+
+INSERT INTO order_items (order_item_id, discount, ordered_product_price, quantity, order_id, product_id) VALUES (3, 500000, 2000000, 1, 1, 3);
+INSERT INTO order_items (order_item_id, discount, ordered_product_price, quantity, order_id, product_id) VALUES (4, 1000000, 5000000, 1, 1, 9);
+INSERT INTO order_items (order_item_id, discount, ordered_product_price, quantity, order_id, product_id) VALUES (5, 200000, -20989500000, 1, 2, 11);
+INSERT INTO order_items (order_item_id, discount, ordered_product_price, quantity, order_id, product_id) VALUES (6, 300000, 1200000, 1, 3, 8);
+INSERT INTO order_items (order_item_id, discount, ordered_product_price, quantity, order_id, product_id) VALUES (7, 200000, -23880000, 1, 3, 11);
+INSERT INTO order_items (order_item_id, discount, ordered_product_price, quantity, order_id, product_id) VALUES (8, 500000, 1000000, 1, 4, 2);
+INSERT INTO order_items (order_item_id, discount, ordered_product_price, quantity, order_id, product_id) VALUES (9, 400000, 1600000, 1, 4, 7);
+INSERT INTO order_items (order_item_id, discount, ordered_product_price, quantity, order_id, product_id) VALUES (10, 200000, 1000000, 3, 5, 10);
+INSERT INTO order_items (order_item_id, discount, ordered_product_price, quantity, order_id, product_id) VALUES (11, 300000, 1500000, 1, 6, 5);
+INSERT INTO order_items (order_item_id, discount, ordered_product_price, quantity, order_id, product_id) VALUES (12, 500000, 2000000, 2, 7, 3);
+INSERT INTO order_items (order_item_id, discount, ordered_product_price, quantity, order_id, product_id) VALUES (13, 1000000, 4000000, 1, 7, 4);
+INSERT INTO order_items (order_item_id, discount, ordered_product_price, quantity, order_id, product_id) VALUES (14, 1000000, 4000000, 3, 8, 4);
+INSERT INTO order_items (order_item_id, discount, ordered_product_price, quantity, order_id, product_id) VALUES (15, 1000000, 4000000, 1, 9, 4);
+INSERT INTO order_items (order_item_id, discount, ordered_product_price, quantity, order_id, product_id) VALUES (16, 1000000, 4000000, 1, 10, 4);
+INSERT INTO order_items (order_item_id, discount, ordered_product_price, quantity, order_id, product_id) VALUES (17, 1000000, 4000000, 1, 11, 4);
+INSERT INTO order_items (order_item_id, discount, ordered_product_price, quantity, order_id, product_id) VALUES (18, 300000, 1500000, 1, 11, 5);
+
+
+--
+-- Data for Name: roles; Type: TABLE DATA; 
+--
+
+INSERT INTO roles (role_id, role_name) VALUES (101, 'ADMIN');
+INSERT INTO roles (role_id, role_name) VALUES (102, 'USER');
+INSERT INTO roles (role_id, role_name) VALUES (1, 'Customer');
+
+
+--
+-- Data for Name: user_address; Type: TABLE DATA; 
+--
+
+INSERT INTO user_address (user_id, address_id) VALUES (1, 8);
+INSERT INTO user_address (user_id, address_id) VALUES (2, 9);
+INSERT INTO user_address (user_id, address_id) VALUES (3, 13);
+INSERT INTO user_address (user_id, address_id) VALUES (4, 10);
+INSERT INTO user_address (user_id, address_id) VALUES (5, 14);
+INSERT INTO user_address (user_id, address_id) VALUES (6, 16);
+INSERT INTO user_address (user_id, address_id) VALUES (7, 15);
+INSERT INTO user_address (user_id, address_id) VALUES (8, 17);
+INSERT INTO user_address (user_id, address_id) VALUES (9, 18);
+INSERT INTO user_address (user_id, address_id) VALUES (10, 22);
+INSERT INTO user_address (user_id, address_id) VALUES (12, 21);
+
+
+--
+-- Data for Name: user_role; Type: TABLE DATA; 
+--
+
+INSERT INTO user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO user_role (user_id, role_id) VALUES (1, 102);
+INSERT INTO user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO user_role (user_id, role_id) VALUES (2, 102);
+INSERT INTO user_role (user_id, role_id) VALUES (3, 102);
+INSERT INTO user_role (user_id, role_id) VALUES (3, 101);
+INSERT INTO user_role (user_id, role_id) VALUES (4, 101);
+INSERT INTO user_role (user_id, role_id) VALUES (4, 102);
+INSERT INTO user_role (user_id, role_id) VALUES (5, 1);
+INSERT INTO user_role (user_id, role_id) VALUES (5, 102);
+INSERT INTO user_role (user_id, role_id) VALUES (6, 1);
+INSERT INTO user_role (user_id, role_id) VALUES (6, 102);
+INSERT INTO user_role (user_id, role_id) VALUES (7, 1);
+INSERT INTO user_role (user_id, role_id) VALUES (7, 102);
+INSERT INTO user_role (user_id, role_id) VALUES (8, 1);
+INSERT INTO user_role (user_id, role_id) VALUES (8, 102);
+INSERT INTO user_role (user_id, role_id) VALUES (9, 1);
+INSERT INTO user_role (user_id, role_id) VALUES (9, 102);
+INSERT INTO user_role (user_id, role_id) VALUES (10, 1);
+INSERT INTO user_role (user_id, role_id) VALUES (10, 102);
+INSERT INTO user_role (user_id, role_id) VALUES (12, 1);
+INSERT INTO user_role (user_id, role_id) VALUES (12, 102);
